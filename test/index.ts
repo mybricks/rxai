e64a3ec97953f4a38436c5f3640e2661fd182302
liubiao03 <liubiao03@kuaishou.com>
2025-11-06 22:01:08 +0800

    refactor: tool

--- test/index.ts
+++ test/index.ts
@@ -67,25 +67,25 @@ window.test = () => {
           model = `openai/gpt-5-mini`
 
 
-          let top_p = 0.4, temperature = 0.4
-          //let model = 'openai/gpt-4o'
-          if (aiRole === 'imageCreator') {
-            model = `google/gemini-2.5-flash-image`
-          } else if (aiRole === 'image') {
-            model = `anthropic/claude-3.7-sonnet`
-            model = `google/gemini-2.5-pro-preview`
-            model = `anthropic/claude-sonnet-4`
-          } else if (aiRole === 'architect') {
-            //temperature = 0.8
-            model = 'openai/gpt-4o-2024-11-20'
-            model = `deepseek/deepseek-r1-0528`
-            model = `google/gemini-2.5-pro`
-          } else if (aiRole === 'expert') {
-            model = `anthropic/claude-3.5-sonnet:beta`
-            model = `google/gemini-2.5-pro-preview`
-            model = `deepseek/deepseek-r1-0528`
-            model = `anthropic/claude-sonnet-4`
-          }
+          // let top_p = 0.4, temperature = 0.4
+          // //let model = 'openai/gpt-4o'
+          // if (aiRole === 'imageCreator') {
+          //   model = `google/gemini-2.5-flash-image`
+          // } else if (aiRole === 'image') {
+          //   model = `anthropic/claude-3.7-sonnet`
+          //   model = `google/gemini-2.5-pro-preview`
+          //   model = `anthropic/claude-sonnet-4`
+          // } else if (aiRole === 'architect') {
+          //   //temperature = 0.8
+          //   model = 'openai/gpt-4o-2024-11-20'
+          //   model = `deepseek/deepseek-r1-0528`
+          //   model = `google/gemini-2.5-pro`
+          // } else if (aiRole === 'expert') {
+          //   model = `anthropic/claude-3.5-sonnet:beta`
+          //   model = `google/gemini-2.5-pro-preview`
+          //   model = `deepseek/deepseek-r1-0528`
+          //   model = `anthropic/claude-sonnet-4`
+          // }
 
           const controller = new AbortController();
           fetch('http://ai.mybricks.world/stream-test', {
