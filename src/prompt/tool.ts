4ca091ba9032c34465ecd53c8e3fa75a884a4d42
leon-js <42924541+leon-js@users.noreply.github.com>
2025-11-11 18:18:43 +0800

    update

--- src/prompt/tool.ts
+++ src/prompt/tool.ts
@@ -1,9 +1,9 @@
 const getToolPrompt = (tool: Tool) => {
-  return `<工具总览>
+  return `<当前调用工具描述>
 你是一个可被调用的工具（${tool.name}），用于${tool.description}。
 请按照下述工具详细描述，完成工具调用。
 ${tool.getPrompts()}
-</工具总览>
+</当前调用工具描述>
 `;
 };
 
