bd57ac96a2d13b3860942fd3decbb040a7baf627
liubiao03 <liubiao03@kuaishou.com>
2025-11-14 11:42:56 +0800

    feat:

--- src/tools/index.ts
+++
@@ -1,9 +0,0 @@
-import GeneratePage from './generate-page';
-import GetComponentsDocAndPrd from './get-components-doc-and-prd';
-import ModifyComponent from './modify-component'
-
-export const MYBRICKS_TOOLS = {
-  GeneratePage,
-  GetComponentsDocAndPrd,
-  ModifyComponent
-}
