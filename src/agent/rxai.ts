c51774a87afce8671fe21122ba1b07765c7cca3f
leon-js <42924541+leon-js@users.noreply.github.com>
2025-12-04 20:51:55 +0800

    update

--- src/agent/rxai.ts
+++ src/agent/rxai.ts
@@ -116,7 +116,7 @@ class Rxai extends BaseAgent {
       system: this.system,
       emits,
       message,
-      historyMessages: this.cacheMessages.slice(-10).reduce((pre, cur) => {
+      historyMessages: this.cacheMessages.slice(-50).reduce((pre, cur) => {
         pre.push(...cur.getMessages());
         return pre;
       }, [] as ChatMessages),
